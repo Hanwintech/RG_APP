6e5db2aa4999707b7011bcb0893ad29e80bc8f5f
Always8023 <31231284+Always8023@users.noreply.github.com>
2017-12-21 09:13:01 +0800

    "案件统计图表"和"巡查统计图表"共完成95%

--- src/pages/search/charts/inspect-chart/inspect-chart.ts
+++ src/pages/search/charts/inspect-chart/inspect-chart.ts
@@ -58,6 +58,138 @@ export class InspectChartPage {
       default:
         break;
     }
-  }
+    let da = [];
+    for (let data of this.dataSource) {
 
+      if (data[0] != "江苏省") {
+        if (data[0] == "全国重点文物保护单位") {
+          this.xAxis.push(
+            "国家级"
+          );
+        } else if (data[0] == "省级文物保护单位") {
+          this.xAxis.push(
+            "省级"
+          );
+        } else if (data[0] == "市级文物保护单位") {
+          this.xAxis.push(
+            "市级"
+          );
+        } else if (data[0] == "县区级文物保护单位") {
+          this.xAxis.push(
+            "县区级"
+          );
+        } else {
+          this.xAxis.push(
+            data[0]
+          );
+        }
+        this.yAxis.push(
+          data[1]
+        );
+      }
+
+      if (data[1] != "0" && data[0] != "江苏省") {
+        da.push(
+          { value: data[1], name: data[0] }
+        );
+      }
+
+    }
+
+    let op1 = {
+      grid: { top: '25%', bottom: '25%' },
+      xAxis: {
+        data: this.xAxis,
+      },
+      dataZoom: [//给x轴设置滚动条  
+        {
+          startValue: 0,
+          endValue: this.endValue,
+          type: 'slider',
+          show: false,
+          xAxisIndex: [0],
+        },
+        //下面这个属性是里面拖到  
+        {
+          type: 'inside',
+          show: true,
+          xAxisIndex: [0],
+          startValue: 0,
+          endValue: this.endValue,
+        },
+      ],
+      yAxis: { name: "巡查次数" },
+      series: []
+    };
+    let op2 = {
+      grid: { top: '25%', bottom: '25%' },
+      xAxis: {
+        data: this.xAxis,
+      },
+      dataZoom: [//给x轴设置滚动条  
+        {
+          startValue: 0,
+          endValue: this.endValue,
+          type: 'slider',
+          show: false,
+          xAxisIndex: [0],
+        },
+        //下面这个属性是里面拖到  
+        {
+          type: 'inside',
+          show: true,
+          xAxisIndex: [0],
+          startValue: 0,
+          endValue: this.endValue,
+        },
+      ],
+      yAxis: { name: "巡查次数" },
+      series: []
+    };
+
+    let op3 = {
+      series: []
+    };
+
+    op1.series.push({
+      name: '巡查次数',
+      type: 'line',
+      label: {
+        normal: { show: true, position: 'top' }
+      },
+      data: this.yAxis
+    });
+
+    op2.series.push({
+      name: '巡查次数',
+      type: 'bar',
+      label: {
+        normal: { show: true, position: 'top' }
+      },
+      data: this.yAxis
+    });
+
+    op3.series.push({
+      name: '巡查次数',
+      type: 'pie',
+      //stillShowZeroSum: false,
+      radius: '55%',
+      center: ['50%', '60%'],
+      itemStyle: {
+        normal: {
+          label: {
+            show: true,
+            formatter: '{b}:{c}',
+            //position: 'inside'
+          },
+          labelLine: { show: false }
+        }
+      },
+      data: da
+    });
+
+    this.option1 = op1;
+    this.option2 = op2;
+    this.option3 = op3;
+  }
 }

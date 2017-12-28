2b3cfcf2248d21148de15f26ca90fdd955cc8cfd
Always8023 <31231284+Always8023@users.noreply.github.com>
2017-12-28 10:58:17 +0800

    “案件统计-条件查询”完成80%

--- src/apis/property/get-museum-info-list.api.ts
+++
@@ -1,7 +0,0 @@
-import { GetListAPI } from './../get-list-api.api';
-
-export class GetMuseumInfoList extends GetListAPI {
-    constructor() {
-        super("/api/system/get_museum_infos");
-    }
-}
